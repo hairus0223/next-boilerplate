d719985929217254ec0b9e1c25e60423c64295d2
Khairus <playsave23@gmail.com>
2024-06-14 10:22:11 +0700

    build: update middleware

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,4 +1,4 @@
-import { Locale } from '@/utils/configs/i18n'
+import { i18n, Locale } from '@/utils/configs/i18n'
 import { ChildrenType } from '@/utils/models/core-model'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -12,8 +12,10 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children, params }: ChildrenType & { params: { lang: Locale } }) {
+  const direction = i18n.langDirection[params.lang]
+
   return (
-    <html lang='en'>
+    <html id='__next' lang={params.lang} dir={direction}>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
 // Config Imports
 import { i18n } from './utils/configs/i18n'
 import { getLocalizedUrl, isUrlMissingLocale } from './utils/helpers/i18n'
-import { ensurePrefix } from './utils/helpers/string'
+import { ensurePrefix, withoutSuffix } from './utils/helpers/string'
 
 // Util Imports
 
@@ -60,16 +60,67 @@ const localizedRedirect = (url: string, locale: string | undefined, request: Nex
   return NextResponse.redirect(redirectUrl)
 }
 
-export default withAuth(async function middleware(request: NextRequestWithAuth) {}, {
-  callbacks: {
-    authorized: () => {
-      // This is a work-around for handling redirect on auth pages.
-      // We return true here so that the middleware function above
-      // is always called.
-      return true
+export default withAuth(
+  async function middleware(request: NextRequestWithAuth) {
+    // Get locale from request headers
+    const locale = getLocale(request)
+
+    const pathname = request.nextUrl.pathname
+
+    // If the user is logged in, `token` will be an object containing the user's details
+    const token = request.nextauth.token
+
+    // Check if the user is logged in
+    const isUserLoggedIn = !!token
+
+    // Guest routes (Routes that can be accessed by guest users who are not logged in)
+    const guestRoutes = ['login', 'register', 'forgot-password']
+
+    // Shared routes (Routes that can be accessed by both guest and logged in users)
+    const sharedRoutes = ['shared-route']
+
+    // Private routes (All routes except guest and shared routes that can only be accessed by logged in users)
+    const privateRoute = ![...guestRoutes, ...sharedRoutes].some(route => pathname.endsWith(route))
+
+    // If the user is not logged in and is trying to access a private route, redirect to the login page
+    if (!isUserLoggedIn && privateRoute) {
+      let redirectUrl = '/login'
+
+      if (!(pathname === '/' || pathname === `/${locale}`)) {
+        const searchParamsStr = new URLSearchParams({ redirectTo: withoutSuffix(pathname, '/') }).toString()
+
+        redirectUrl += `?${searchParamsStr}`
+
+        return localizedRedirect(redirectUrl, locale, request)
+      }
+    }
+
+    // If the user is logged in and is trying to access a guest route, redirect to the root page
+    const isRequestedRouteIsGuestRoute = guestRoutes.some(route => pathname.endsWith(route))
+
+    if (isUserLoggedIn && isRequestedRouteIsGuestRoute) {
+      return localizedRedirect(HOME_PAGE_URL, locale, request)
+    }
+
+    // If the user is logged in and is trying to access root page, redirect to the home page
+    if (pathname === '/' || pathname === `/${locale}`) {
+      return localizedRedirect(HOME_PAGE_URL, locale, request)
+    }
+
+    // If pathname already contains a locale, return next() else redirect with localized URL
+    return isUrlMissingLocale(pathname) ? localizedRedirect(pathname, locale, request) : NextResponse.next()
+  },
+  {
+    callbacks: {
+      authorized: () => {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+        return true
+      }
     }
   }
-})
+)
 
 // Matcher Config
 export const config = {
